eecbb6a6ac0d60383c064f663195b37b2a3ee950
Ihor Lutskyy <igor.lutskiy@gmail.com>
2018-09-12 21:56:04 -0700

    Tasks implementartion

---
+++ tasks/webPackage/data.js
@@ -0,0 +1,14 @@
+let data = {
+  "main.html":[
+    "main.js","main.css","main.jpg"
+  ],
+  "main.css":[
+    "reset.css", "responsive.css", "grid.css"
+  ],
+  "responsive.css": [
+    "desktop.css", "mobile.css"
+  ],
+  "about.html": [
+    "about.css", "reset.css", "responsive.css"
+  ]
+};
